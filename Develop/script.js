e441a0345b7083c7a7f5bd9455c520acbe7cb377
Manuelacreate <107735377+Manuelacreate@users.noreply.github.com>
2022-07-20 18:55:30 -0700

    started password generator project

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,45 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+
+  var length = parseInt(prompt("How long would you like your password to be?"))
+console.log (length)
+
+if (Number.isNaN(length)) {
+  alert("Please enter a valid number.")
+  return null
+}
+
+if (length <8) {
+  alert("Make sure your password is larger than 8 characters!")
+  return null
+}
+
+if (length >128) {
+  alert("Please choose a number that is less than 128 characters!")
+  return null
+}
+
+
+
+var includeNumber = confirm("Click the ok button if you want numbers in your password")
+
+var uppercase = (confirm("Would you like an uppercase in your password?"))
+console.log (uppercase)
+
+var lowercase = (confirm("Would you like a lowercase in your password?"))
+console.log (lowercase)
+
+var specialcharacter = (confirm("Would you like a special character?"))
+console.log (specialcharacter)
+}
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
